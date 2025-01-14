d6a6b74aafe234346f56421939db023844650eb7
kevin <k-hr123@hotmail.com>
2025-01-14 17:11:46 -0500

    agregando migracion productos

--- coffe_shop/productos/models.py
+++ coffe_shop/productos/models.py
@@ -7,3 +7,13 @@ class Product(models.Model):
                                                                     #  y mostrarlo como queremos
     description = models.TextField(max_length=200, verbose_name='descripcion')
     price = models.DecimalField(max_digits=10, decimal_places=2, verbose_name='precio')  
+    available = models.BooleanField(default=True, verbose_name='disponible')
+    photo = models.ImageField(upload_to="logos",null=True, blank=True, verbose_name='foto')  
+                            #--> argumento donde especifica donde se sube la foto
+                            #--> permitimos que se puedan cargar productos sin foto con los otros argumentos
+                            #--> ponemos nombre al campo 'foto'
+
+    def __str__(self):
+        return self.name
+    
+    
