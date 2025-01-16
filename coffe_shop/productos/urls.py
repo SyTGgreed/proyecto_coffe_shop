7149d035835316847d7b6e9d4e8f483cb3a182c9
kevin <k-hr123@hotmail.com>
2025-01-16 16:54:32 -0500

    configurando uls y vista, agregando dependencia y extension sqlite3

--- coffe_shop/productos/urls.py
+++ coffe_shop/productos/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path, include
+from django.urls import path
+from .views import ListProductsViews
+
+
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('lista_productos/', include('productos.urls'))
+    path('list_products/', ListProductsViews.as_view(), name='list'),
+
 ]
--- coffe_shop/productos/views.py
+++ coffe_shop/productos/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .models import Product
+from django.views.generic import TemplateView
 
 # Create your views here.
+
+class ListProductsViews(TemplateView):
+    template_name = "coffe_shop/productos/coffe_shop/productos.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        products = Product.objects.all()
+        context["list_products"] = products
+
+        return context
+        
+
+
+
+
