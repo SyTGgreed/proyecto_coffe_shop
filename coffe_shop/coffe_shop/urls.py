4a84d462691199aa0c403a02e0d123ab3b2811c0
kevin <k-hr123@hotmail.com>
2025-02-13 19:55:08 -0500

    añadiendo imagenes

--- coffe_shop/coffe_shop/urls.py
+++ coffe_shop/coffe_shop/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('lista_productos/', include('productos.urls')),
 
-]
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)   # static() --> para obtener la url de los archivos estaticos
+        # recibe como parametros la configuracion que tenemos de los archivos estaticos y de media
+        # importando settings 
--- coffe_shop/productos/admin.py
+++ coffe_shop/productos/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Product
 
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):  # ModelAdmin nos permite crear o registrar modelos desde el administrador
+    model = Product       # le pasamos el modelo
+    list_display = ['name', 'price']   # nos permite listar por los atributos del modelo producto
+    search_fields = ['name']    # filtra y coloca un buscador, recibe como parametros los parametros del producto
+
+admin.site.register(Product, ProductAdmin)
